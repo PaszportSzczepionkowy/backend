4e64d7d467468873d7f6d2336423d609587d1c1f
vjasieg <vjasieg@gmail.com>
2021-05-31 14:45:04 +0200

    /szczepionka/get added (basic)

--- routes/szczepionka/get/szczepionka_get.js
+++ routes/szczepionka/get/szczepionka_get.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var szczepionka_model = require("../../../models/szczepionka_model")
 
 router.get('/', function(req, res, next) {
+    if(!(req.body.password === process.env.ADMIN_PASS)) {
+        res.status(401).json({
+            "result": "Złe hasło."
+        })
+    }
+    szczepionka_model.find({}).exec().then(result => {
+        res.status(200).json(result)
+    }).catch(err => {
 
+    })
 });
 
 module.exports = router;
